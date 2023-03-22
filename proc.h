53e88ff5ddc2e94dcffae6386c3dca471871a7e0
akshaykhoje <khojeakshay02@gmail.com>
2023-03-23 00:23:11 +0530

    added initial notes from demo

--- proc.h
+++ proc.h
@@ -49,6 +49,8 @@ struct proc {
   struct file *ofile[NOFILE];  // Open files
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
+  //thread *children;            // Akshay_kts : create a hierarchy with parent and child pointers
+  //thread *parent;              // Akshay_kts
 };
 
 // Process memory is laid out contiguously, low addresses first:
